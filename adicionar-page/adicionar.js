91dd0e6a35efdd1279c80c8a70a463261e64460f
Gabriel Trindade <gabrielt20514@gmail.com>
2025-08-03 02:54:37 -0300

    feat: js pagina adicionar

---
+++ adicionar-page/adicionar.js
@@ -0,0 +1,52 @@
+const API_URL = 'http://localhost:3000/products';
+
+const formAdicionar = document.querySelector('form');
+const nomeInput = document.getElementById('nome');
+const imagemInput = document.getElementById('image');
+const descricaoInput = document.getElementById('descricao');
+const precoInput = document.getElementById('preco');
+const categoriasInput = document.getElementById('categorias');
+const avaliacaoInput = document.getElementById('avaliacao');
+
+formAdicionar.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const novoProduto = {
+        name: nomeInput.value,
+        image: imagemInput.value,
+        description: descricaoInput.value,
+        price: parseFloat(precoInput.value),
+        category: categoriasInput.value,
+        inStock: true, 
+        rating: parseFloat(avaliacaoInput.value),
+    };
+
+    if (!novoProduto.name || !novoProduto.image || !novoProduto.description || isNaN(novoProduto.price) || !novoProduto.category || isNaN(novoProduto.rating)) {
+        alert('Por favor, preencha todos os campos corretamente.');
+        return;
+    }
+
+    try {
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(novoProduto),
+        });
+
+        if (response.ok) {
+            alert('Produto adicionado com sucesso!');
+            const totalProductsResponse = await fetch(API_URL);
+            const totalCount = totalProductsResponse.headers.get('X-Total-Count');
+            const limitePorPagina = 5; 
+            const ultimaPagina = Math.ceil(totalCount / limitePorPagina);
+            window.location.href = `../homepage/index.html${ultimaPagina}`;
+        } else {
+            alert('Ocorreu um erro ao adicionar o produto. Status: ' + response.status);
+        }
+    } catch (error) {
+        console.error('Erro na requisição:', error);
+        alert('Ocorreu um erro de conexão. Verifique se o json-server está rodando.');
+    }
+});
